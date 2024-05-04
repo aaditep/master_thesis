fc9d24e8f98f528a87df155ee3a2e031c901c903
Aadi Tepper <atepper@eu-login-20.euler.ethz.ch>
2024-05-04 15:52:39 +0200

    Simclr_kids450.py

--- SimCLR/Simclr_kids450.py
+++ SimCLR/Simclr_kids450.py
@@ -1,7 +1,5 @@
 import numpy as np
-import pandas as pd
-import shutil, time, os, requests, random, copy, glob
-import pickle
+import time, os, glob
 import h5py
 from collections import defaultdict
 import torch
@@ -13,7 +11,7 @@ from torchsummary import summary
 from my_utils.LARS import LARS
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
-from my_utils.Dataloader_class import Kids450
+from my_utils.Dataloader_class import Kids450, kids450_files_localscratch
 from my_utils.NT_xent_loss import SimCLR_Loss
 import yaml
 
@@ -22,69 +20,7 @@ import torch.multiprocessing
 #torch.multiprocessing.set_sharing_strategy('file_system')
 
 
-
-
-def collate__faster_load(idxs):
-        """
-        Faster collate function for the dataloader. It loads the images and then augments them.
-        The faster part is loading images belonging to the same file, not opening and closing the file for each image.
-
-        Args:
-            idxs: list of indices
-        Returns:
-            x1_batch: batch of augmented(if training phase) images
-            x2_batch: batch of augmented(if training phase) images
-        """
-        
-        batch_size = len(idxs)
-        
-        x1_indices = [item[0] for item in idxs] #list of  tuples (file_idx, element_idx_within_file)
-        x2_indices = [item[1] for item in idxs] #element number from randomized lookup table to use for taking image from same cosmology/file
-        
-        #group the element indeces belonging to a certain file in order to
-        file_groups = defaultdict(list)
-        for file_index, element_index in x1_indices:
-            file_groups[file_index].append(element_index)
-        # Convert defaultdict to regular dictionary and sort by file index
-        #sort by file and elements
-        sorted_file_groups = sorted(file_groups.items())
-
-       
-        resolution = 128
-        #create empty tensors for batches
-        x1_batch = torch.empty((batch_size,1 ,4, resolution, resolution)) #the 1 is added as tensor.unsqueeze(1)
-        x2_batch = torch.empty((batch_size,1 ,4, resolution, resolution)) #the 1 is added as tensor.unsqueeze(1)
-
-        count = 0#count for adding elements to batch and take x2
-        for file_idx, elements in sorted_file_groups:  #extract file idx and corrseponding elements
-            with h5py.File(file_paths[file_idx], "r") as f:
-                data = f["kappa"]
-                for elem in elements:
-
-                    x1 = data[elem] #get element from file
-                    x2 = data[x2_indices[count]] #get different element from same file
-                    
-                    #normalize
-                    x1 = x1/255.0# why it divides by 255.0, beacuse original images in 0-255 range of pixel values then you get it to [0,1]
-                    x2 = x2/255.0
-                    
-                    #augment
-                    x1 = dg.augment(torch.from_numpy(x1))# torch.from_numpy() changes into tensor in place wp copy
-                    x2 = dg.augment(torch.from_numpy(x2)) 
-                    
-                    x1 = x1.unsqueeze(0) #to add 1 dimesion to fit the original batch shape
-                    x2 = x2.unsqueeze(0) #to add 1 dimesion to fit the original batch shape
-                    
-                    
-                    #add to batch
-                    x1_batch[count] = x1
-                    x2_batch[count] = x2
-            
-                    #add to counter
-                    count +=1
-
-                    #torch.from_numpy(x1)
-        return x1_batch, x2_batch
+from my_utils.collate_file_funcs import collate_fn
 
 
 #Set random seed for reproducibility
@@ -206,23 +142,6 @@ class PreModel(nn.Module):
     
 #Extra functions for model saving and loading and pltting features with TSNE
 
-def save_model(model, optimizer, scheduler, current_epoch, name):
-    """
-    Save the model
-
-    Args:
-    model : PreModel : model
-    optimizer : torch.optim.Optimizer : optimizer
-    scheduler : torch.optim.lr_scheduler : scheduler
-    current_epoch : int : current epoch
-    name : str : name of the model
-    """
-    out = os.path.join('./data/saved_models/',name.format(current_epoch))
-
-    torch.save({'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'scheduler_state_dict':scheduler.state_dict()}, out)
-
 def plot_features(model, num_classes, num_feats, batch_size, current_epoch):
     """
     Plot the features with TSNE. THIS IS STILL A TODO-IMPLEMENTATION
@@ -261,27 +180,6 @@ def plot_features(model, num_classes, num_feats, batch_size, current_epoch):
     plt.savefig(save_path+plt_name, format='pdf')#save plot
     #plt.show()
 
-def plot_losses(tr_loss, val_loss,current_epoch):
-    """
-    Plot the training and validation loss
-
-    Args:
-    tr_loss : list : training loss
-    val_loss : list : validation loss
-    current_epoch : int : current epoch
-    """
-    plt.clf()
-    plt.plot(tr_loss)
-    plt.plot(val_loss)
-    plt.title(f"Training and Validation Loss at epoch {current_epoch}")
-    plt.xlabel("Epochs")
-    plt.ylabel("Loss")
-    plt.legend(["Training Loss","Validation Loss"])
-    save_path = "./data/plots/"
-    plt_name= f"Loss_plot_epoch_{current_epoch}.pdf"
-    plt.savefig(save_path+plt_name, format='pdf')
-    #plt.show()
-    
 def load_config(config_file):
     """Load YAML configuration from file."""
     with open(config_file, "r") as yaml_file:
@@ -289,24 +187,7 @@ def load_config(config_file):
     return config
 
 
-def data_origin(origin : str, full_sample = True):
-    """
-    Get the data origin
-
-    Args:
-    origin : str : origin of the data
-    full_sample : bool : use full sample or not (5% or 100% of data)
-    """
-    if origin == 'local_scratch':
-        tmp_dir = os.environ.get('TMPDIR') # Get the list of input files at local scratch
-        input_directory = tmp_dir + "/kids_450_h5"
-        if full_sample == False:
-            input_directory = tmp_dir + "/kids_450_h5_small_sample"
-        file_pattern = '*train.h5'
-        file_paths = glob.glob(os.path.join(input_directory, file_pattern))
-        return file_paths
-
-def prepare_datasets(file_paths : list):
+def prepare_datasets(file_paths : list, resolution : int):
     """
     Prepare the datasets for training and validation data
 
@@ -317,8 +198,8 @@ def prepare_datasets(file_paths : list):
     dg : Kids450 : dataset for training data
     vdg : Kids450 : dataset for validation data
     """
-    dg = Kids450(phase = "train",file_paths = file_paths)
-    vdg = Kids450(phase = "val",file_paths = file_paths)
+    dg = Kids450(phase = "train",file_paths = file_paths, resolution = resolution )
+    vdg = Kids450(phase = "val",file_paths = file_paths, resolution = resolution)
     return dg, vdg
 
 
@@ -339,14 +220,14 @@ def prepare_dataloaders(dg, vdg, batch_size: int, num_workers: int):
     train_loader = DataLoader(dg,batch_size = batch_size,
                                drop_last=True,
                                prefetch_factor = 3,
-                               collate_fn = collate__faster_load,
+                               collate_fn = collate_fn,
                                pin_memory = True,
                                num_workers = num_workers)
 
     valid_loader = DataLoader(vdg,batch_size = batch_size,
                                drop_last=True,
                                prefetch_factor = 3,
-                               collate_fn = collate__faster_load,
+                               collate_fn = collate_fn,
                                pin_memory = True,
                                num_workers = num_workers)
 
@@ -359,39 +240,41 @@ class Trainer:
     def __init__(
         self,
         model,
+        dg,
         train_data,
+        valid_data,
         optimizer,
         gpu_id,
         warmupscheduler,
         mainscheduler,
         criterion,
-        save_every, 
-    ) -> None:
+        save_every,
+        run_name 
+    ) -> None: # the -> None is a return type "hint" that literally tells us that the function returns nothing
         self.gpu_id = gpu_id
         self.model = model.to(gpu_id)
+        self.dataset_class = dg
         self.train_data = train_data
+        self.valid_data = valid_data
         self.optimizer = optimizer
         self.warmupscheduler = warmupscheduler
         self.mainscheduler = mainscheduler
         self.loss = criterion
         self.save_every = save_every
+        self.run_name = run_name
 
-    def _run_batch(self, x_i, x_j):
-        """
-        Run a single batch through the model and update the weights
+        self.total_batches = len(self.train_data)
+        self.example_ct = 0 # number of examples seen
+        self.batch_ct = 0 # number of batches seen
 
-        Args:
-        x_i : torch.Tensor : input data
-        x_j : torch.Tensor : input data
-        """
-        self.optimizer.zero_grad()
-         # positive pair, with encoding
-        z_i = self.model(x_i)
-        z_j = self.model(x_j)
-        loss = self.loss(z_i, z_j)
-        loss.backward()
-        self.optimizer.step()
 
+        self.stime = 0
+        self.tr_loss =[]
+        self.tr_loss_epoch = 0
+
+
+
+    #I will try to combine the run_batch and run_epoch functions. It is too much to have them separate
     def _run_epoch(self, epoch):
         """
         Run a single epoch through the model
@@ -401,37 +284,92 @@ class Trainer:
         """
         b_sz = len(next(iter(self.train_data))[0]) #get batch size
         print(f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
-        for (x_i,x_j) in self.train_data:
+        for step,(x_i,x_j) in enumerate(self.train_data):
             x_i = x_i.squeeze().to(self.gpu_id).float()
             x_j = x_j.squeeze().to(self.gpu_id).float()
-            self._run_batch(x_i, x_j)
+            
+            self.optimizer.zero_grad()
+    
+            z_i = self.model(x_i)
+            z_j = self.model(x_j)
 
+            loss = self.loss(z_i, z_j)
+            loss.backward()
 
+            self.example_ct += b_sz
+            self.batch_ct += 1
 
-    def _save_checkpoint(self, epoch):
-        ckp = self.model.state_dict()
-        PATH = "./data/saved_models_multi/checkpoint.pt"
-        torch.save(ckp, PATH)
-        print(f"Epoch {epoch} | Training checkpoint saved at {PATH}")
+            self.optimizer.step()
+            
+            if step % 50 == 0: #log loss at every 50th step
+                print(f"Step [{step}/{len(self.train_data)}]\t Loss: {round(loss.item(), 5)}",flush = True)
+                wandb.log({"epoch": epoch, "loss": loss}, step=self.example_ct)
+            self.tr_loss_epoch += loss.item()
+
+            
+
+    def save_model(self,model, optimizer, scheduler, current_epoch, run_name):
+        """
+        Save the model
+
+        Args:
+        model : PreModel : model
+        optimizer : torch.optim.Optimizer : optimizer
+        scheduler : torch.optim.lr_scheduler : scheduler
+        current_epoch : int : current epoch
+        run_name : str : name of the model
+        """
+        run_name = run_name + f"_epoch_{current_epoch}.pt"
+        out = os.path.join('./data/saved_models/',run_name)
+
+        torch.save({'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'scheduler_state_dict':scheduler.state_dict()}, out)
+
+    
 
     def train(self, max_epochs: int):
+        """
+        Train the model. learning rate schedulers are used for warmup and cosine decay depending on the epoch
+        Args:
+        max_epochs : int : maximum epochs
+        """
+         # Tell wandb to watch what the model gets up to: gradients, weights, and more!
+        wandb.watch(self.model, self.loss, log="all", log_freq=50)
         for epoch in range(max_epochs):
+            epoch += 1 #start from 1
+            self.tr_loss_epoch = 0
+            self.stime = time.time()
             self._run_epoch(epoch)
             if epoch < 10:
                 self.warmupscheduler.step()
-            else:
+
+            if epoch >= 10:
                 self.mainscheduler.step()
+
             if epoch % self.save_every == 0:
-                self._save_checkpoint(epoch)
+                self.save_model(self.model, self.optimizer, self.mainscheduler , epoch, self.run_name)
+                 
+
+
+            self.tr_loss.append(self.tr_loss_epoch / len(self.train_data))
+            loss_per_epoch = self.tr_loss_epoch / len(self.train_data)
+            time_taken = (time.time()-self.stime)/60
+            print(f"Epoch [{epoch}/{max_epochs}]\t Training Loss: {loss_per_epoch}\t Time Taken: {time_taken} minutes",flush = True)
+            #print(f"Epoch [{epoch}/{max_epochs}]\t Time Taken: {time_taken} minutes",flush = True) 
+            wandb.log({"Time taken per epoch": time_taken, "Training loss per epoch": loss_per_epoch})
+            #I want to log this loss to wandb so avg loss per epoch
+            self.dataset_class.on_epoch_end()
+            #dg.on_epoch_end()#shuffle data inside each epoch ##TODO CHeck if this is uses it actually works
 
 
 
-def load_train_objs(batch_size):
+def load_train_objs(config, file_paths):
     """
     Load the training objects
 
     Args:
-    batch_size : int : batch size
+    batch_size : int : batch sizewww
     
     Returns:
     model : PreModel : model
@@ -440,8 +378,9 @@ def load_train_objs(batch_size):
     mainscheduler : torch.optim.lr_scheduler.CosineAnnealingWarmRestarts : main scheduler
     criterion : SimCLR_Loss : loss function
     """
-
-    model = PreModel('resnet18').to("cuda")
+    dg, vdg = prepare_datasets(file_paths, config.resolution)
+    train_loader, valid_loader = prepare_dataloaders(dg, vdg, config.batch_size, config.num_workers)
+    model = PreModel("resnet18").to("cuda")
     print(f" Summary of the model: {summary(model, (4, 128, 128))}",flush=True)# Works now
     optimizer = LARS(
                     [params for params in model.parameters() if params.requires_grad],
@@ -451,12 +390,12 @@ def load_train_objs(batch_size):
                     )
     warmupscheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lambda epoch : (epoch+1)/10.0, verbose = True) #decay the learning rate with the cosine decay schedule without restarts"
     mainscheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, 500, eta_min=0.05, last_epoch=-1, verbose = True)#scheduler for cosine decay
-    criterion = SimCLR_Loss(batch_size = batch_size, temperature = 0.5)#loss function
+    criterion = SimCLR_Loss(batch_size = config.batch_size, temperature = 0.5)#loss function
     
     
-    return model, optimizer, warmupscheduler, mainscheduler, criterion
+    return model,dg,train_loader, valid_loader,optimizer, warmupscheduler, mainscheduler, criterion
 
-def main(device, total_epochs,save_every, batch_size):
+def main(device, file_paths, config):
     """
     Main function to train the model
 
@@ -466,9 +405,9 @@ def main(device, total_epochs,save_every, batch_size):
     save_every : int : save every
     batch_size : int : batch size
     """
-    model, optimizer, warmupscheduler, mainscheduler, criterion = load_train_objs(batch_size)
-    trainer = Trainer(model, train_loader, optimizer, device, warmupscheduler, mainscheduler, criterion, save_every)
-    trainer.train(total_epochs)
+    model,dg,train_loader, valid_loader,optimizer, warmupscheduler, mainscheduler, criterion = load_train_objs(config,file_paths)
+    trainer = Trainer(model, dg,train_loader, valid_loader, optimizer, device, warmupscheduler, mainscheduler, criterion, config.save_every, config.run_name)
+    trainer.train(config.total_epochs)
 
 
 if __name__ == "__main__":
@@ -480,12 +419,17 @@ if __name__ == "__main__":
     ######With YAML CONFIG######################
     config_path = args.config
     config = load_config(config_path)
-    file_paths = data_origin(config['origin'], config['full_sample'])
-    dg, vdg = prepare_datasets(file_paths)
-    train_loader, valid_loader = prepare_dataloaders(dg,vdg, config['batch_size'], config['num_workers'])
-
-
-    device = 0  # shorthand for cuda:0
-    main(device, config['total_epochs'],config['save_every'], config['batch_size'])
+    file_paths = kids450_files_localscratch()
+    resolution = config["resolution"]
+    #dg, vdg = prepare_datasets(file_paths, resolution)
+    #Integrate weights and biases
+    import wandb
+    wandb.login()
+    # tell wandb to get started
+    with wandb.init(project="simclr-kids450", config=config):
+        # access all HPs through wandb.config, so logging matches execution!
+        config = wandb.config 
+        device = 0  # shorthand for cuda:0
+        main(device,file_paths, config)
 
     
